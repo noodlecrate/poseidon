e1d7e3b81c93b1af30e2aadbb6008dda00e5f881
James Monger <jameskmonger@hotmail.co.uk>
2016-06-14 23:47:17 +0100

    Configure session endpoint

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,4 +4,9 @@ export function setup (app: any, passport: any) {
 
     // app.post('/session', do all our passport stuff here);
 
+    app.post('/session', passport.authenticate('local-login', {
+        successRedirect : '/profile',
+        failureRedirect : '/login'
+    }));
+
 }
