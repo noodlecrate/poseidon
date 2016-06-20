f9a9c8654404315849f6127a01596f1f89f6a371
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-06-20 18:23:57 +0100

    Add reviews to review repository

--- src/repositories/review-repository.ts
+++ src/repositories/review-repository.ts
@@ -13,6 +13,30 @@ export class ReviewRepository implements IReviewRepository {
         this._userRepository = userRepository;
 
         this._reviews = [];
+
+        this._reviews.push(<ReviewModel> {
+            id: 1,
+            author: this._userRepository.getById(1),
+            title: "Great noodles, top class.",
+            body: "These really have to be among some of the best noodles I've ever tasted. Their noodliness was as noodly as it gets.",
+            imageUrl: "https://i.ytimg.com/vi/XutaTTNihe0/hqdefault.jpg"
+        });
+
+        this._reviews.push(<ReviewModel> {
+            id: 2,
+            author: this._userRepository.getById(2),
+            title: "Not a fan",
+            body: "noodles were too spicy for my tongue",
+            imageUrl: "noodles were too spicy for my tongue"
+        });
+
+        this._reviews.push(<ReviewModel> {
+            id: 3,
+            author: this._userRepository.getById(3),
+            title: "They were alright, I suppose...",
+            body: "They weren't the best, but they weren't the worst. Food's food, I guess.",
+            imageUrl: "http://i798.photobucket.com/albums/yy262/expressionofmyemotions/tumblr_lrud0coRjU1qhsw0o.gif"
+        });
     }
 
     public getAll(): Array<ReviewModel> {
