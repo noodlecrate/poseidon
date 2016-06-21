e359d8afa7599deb5dc52e57ccd870e6f85ca70e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-06-21 13:52:54 +0100

    Bind passport to value

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -38,6 +38,7 @@ kernel.bind<IReviewSerializer>("IReviewSerializer").to(ReviewSerializer);
 kernel.bind<IAuthenticationManager>("IAuthenticationManager").to(AuthenticationManager);
 kernel.bind<IReviewManager>("IReviewManager").to(ReviewManager);
 kernel.bind<IUserManager>("IUserManager").to(UserManager);
+
 kernel.bind<IPassport>("IPassport").toValue(passport);
 
 export default kernel;
