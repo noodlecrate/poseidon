ae342bd3322cb3f0fb9c421b2af563ee217da759
James Monger <jameskmonger@hotmail.co.uk>
2016-06-21 23:30:32 +0100

    Interface between services

---
+++ src/services/_interfaces/service.i.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../../typings/index.d.ts"/>
+
+import { Express } from "express-serve-static-core";
+
+export interface IService {
+
+    registerRoutes (app: Express): void;
+
+}
