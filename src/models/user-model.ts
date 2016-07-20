ac78ca4510b1b7e74171376be42a80496d45cfa7
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-20 20:07:14 +0100

    Lint models

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,12 +1,12 @@
-import { IModel } from "./_namespace";
+import { IModel } from './_namespace';
 
 export class UserModel implements IModel {
 
-    id: number;
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    profileImage: string;
+    public id: number;
+    public username: string;
+    public password: string;
+    public firstName: string;
+    public lastName: string;
+    public profileImage: string;
 
 }
