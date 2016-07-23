6810b73bb6d6d45456e84683e046b7300714435a
James Monger <jameskmonger@hotmail.co.uk>
2016-07-23 22:18:53 +0100

    Create Brand DTO

--- src/dtos/_namespace.ts
+++ src/dtos/_namespace.ts
@@ -1,9 +1,11 @@
 import { ReviewDto } from './review-dto';
 import { UserDto } from './user-dto';
 import { NoodleDto } from './noodle-dto';
+import { BrandDto } from "./brand-dto";
 
 export {
     ReviewDto,
     UserDto,
-    NoodleDto
+    NoodleDto,
+    BrandDto
 };
---
+++ src/dtos/brand-dto.ts
@@ -0,0 +1,6 @@
+export class BrandDto {
+
+    public id: number;
+    public name: string;
+
+}
