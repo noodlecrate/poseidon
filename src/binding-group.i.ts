02d4b4a48d816493741185422dfc6a133036b45e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-26 18:19:09 +0100

    Glob the binding files

--- src/binding-group.i.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='../node_modules/inversify-dts/inversify/inversify.d.ts' />
-
-import * as Inversify from 'inversify';
-
-export interface IBindingGroup {
-
-    registerBindings(kernel: Inversify.interfaces.Kernel): void;
-
-}
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,21 +1,20 @@
 /// <reference path='../node_modules/inversify-dts/inversify/inversify.d.ts' />
 
 import { Kernel } from 'inversify';
-
-import { BindingGroup as ManagerBindingGroup } from './managers/_bindings';
-import { BindingGroup as RepositoryBindingGroup } from './repositories/_bindings';
-import { BindingGroup as SerializerBindingGroup } from './serializers/_bindings';
-import { BindingGroup as ServiceBindingGroup } from './services/_bindings';
+import * as glob from 'glob';
 
 import { IPassport } from './managers/_namespace';
 import * as passport from 'passport';
 
 let kernel = new Kernel();
 
-new ManagerBindingGroup().registerBindings(kernel);
-new RepositoryBindingGroup().registerBindings(kernel);
-new SerializerBindingGroup().registerBindings(kernel);
-new ServiceBindingGroup().registerBindings(kernel);
+glob('./**/_bindings', (e: Error, filePaths: Array<string>) => {
+    filePaths.forEach((filePath: string) => {
+        // grab the binding group's exported function
+        let registerGroupBindings: Function = require(filePath);
+        registerGroupBindings(kernel);
+    });
+});
 
 kernel.bind<IPassport>('IPassport').toConstantValue(passport);
 
