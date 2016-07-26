4147c9f2788c1519e49d76c07e0492cb5c532727
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-26 18:29:23 +0100

    Glob them all!

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,19 +1,24 @@
 /// <reference path='../node_modules/inversify-dts/inversify/inversify.d.ts' />
 
 import { Kernel } from 'inversify';
-import * as glob from 'glob';
+import * as Glob from 'glob';
+import * as path from 'path';
 
 import { IPassport } from './managers/_namespace';
 import * as passport from 'passport';
 
 let kernel = new Kernel();
 
-glob('./**/_bindings', (e: Error, filePaths: Array<string>) => {
-    filePaths.forEach((filePath: string) => {
-        // grab the binding group's exported function
-        let registerGroupBindings: Function = require(filePath);
-        registerGroupBindings(kernel);
-    });
+// create a glob to match all binding paths
+let bindingGlob = path.join(process.cwd(), './**/_bindings.js');
+
+// get all the paths
+let bindingFilePaths = Glob.sync(bindingGlob);
+
+bindingFilePaths.forEach((filePath: string) => {
+    // grab the binding group's exported function
+    let registerGroupBindings: Function = require(filePath).default;
+    registerGroupBindings(kernel);
 });
 
 kernel.bind<IPassport>('IPassport').toConstantValue(passport);
