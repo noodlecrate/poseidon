d5e533359c6fb9be05c63669de0a889c127cd06e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-27 18:10:10 +0100

    Don't export server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as Server from './server';
 import * as DTOsNamespace from './dtos/_namespace';
 import * as ManagersNamespace from './managers/_namespace';
 import * as ModelsNamespace from './models/_namespace';
@@ -8,7 +7,6 @@ import * as ServicesNamespace from './services/_namespace';
 
 exports = {
 
-    Server: Server,
     DTOs: DTOsNamespace,
     Managers: ManagersNamespace,
     Models: ModelsNamespace,
