596c9d2ce8199a6088d1fbcf7d32c1a6d31edc5f
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-28 17:03:28 +0100

    Remove from index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import * as DTOsNamespace from './dtos/_namespace';
 import * as ManagersNamespace from './managers/_namespace';
-import * as ModelsNamespace from './models/_namespace';
 import * as RepositoriesNamespace from './repositories/_namespace';
 import * as SerializersNamespace from './serializers/_namespace';
 import * as ServicesNamespace from './services/_namespace';
 
 export {
 
-    DTOsNamespace as DTOs,
     ManagersNamespace as Managers,
-    ModelsNamespace as Models,
     RepositoriesNamespace as Repositories,
     SerializersNamespace as Serializers,
     ServicesNamespace as Services
